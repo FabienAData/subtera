bf3a46e009370f01f630bcd4412f82689e50405b
Fabien AGNUS <fabien.agnus@sncf.fr>
2020-05-07 18:51:01 +0200

    IMPL: audio files and any imae extension

--- map_viz/map_vizualizer.py
+++ map_viz/map_vizualizer.py
@@ -5,6 +5,8 @@ from folium import LayerControl
 from folium.plugins import MarkerCluster, FeatureGroupSubGroup
 
 import branca
+import base64
+import re
 
 from modules.config.configuration import Configuration
 from data_factory.gold_data_builder import GoldDataBuilder
@@ -37,21 +39,39 @@ class MapVizualizer():
             sub_data = data[data[grouping_feature] == grouping_value]
             for _,values in sub_data.iterrows():
                 name = values['name']
-                id = values['artist_id']
+                id = str(values['artist_id'])
                 living_dates = ' - '.join([str(values["birth_date"]), str(values["death_date"])]) 
                 place = values['birth_place']
                 bio = values['biography']
-                html = f'<b>{name}</b> ({living_dates})<br>Lieu : {place}'
-                png_file = "".join([id, '.png'])
                 resized_images_folder = os.path.join(
                     self._config.processed_images_path, 'resized'
                 )
-                if png_file in os.listdir(resized_images_folder):
-                    img_handler = ImageHandler(png_file, 'resized', self._config)
-                    decoded_image = img_handler.get_decoded_base64_png()
-                    image_html = f'<img src="data:image/png;base64,{decoded_image}">'
-                    html = "<br><br>".join([html, image_html])
+                img_html_tag = ''
+                for img_file in os.listdir(resized_images_folder):
+                    if id in img_file:
+                        img_handler = ImageHandler(img_file, 'resized', self._config)
+                        decoded_image = img_handler.get_decoded_base64_png()
+                        img_extension = re.sub(rf'{id}\.', '', img_file)
+                        img_html_tag = f'<img src="data:image/{img_extension};base64,{decoded_image}">'
 
+
+                audio_path = "/home/agnusfabien/Bureau/Personnel/Projets/projekto/eminem_64.mp3"
+
+                encoded_audio = base64.b64encode(open(audio_path, 'rb').read())
+                decoded_audio = encoded_audio.decode('UTF-8')
+                html = f"""
+                <html>
+                <body>
+                <b>{name}</b>({living_dates})<br>Lieu : {place}
+                <br><br>
+                {img_html_tag}
+                <br><br>
+                <audio controls>
+                <source src="data:audio/mp3;base64, {decoded_audio}"></audio>
+
+                </body>
+                </html>
+                """
                 iframe = branca.element.IFrame(html=html, width=500, height=300)
                 popup = folium.Popup(iframe, max_width=2650)
 
