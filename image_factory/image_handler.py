6c0b3599988cc74293fc13fd4bb5c81667c6e3c2
Fabien AGNUS <fabien.agnus@sncf.fr>
2020-05-07 21:30:56 +0200

    FIX: decompose image_name into name and extension.

--- image_factory/image_handler.py
+++ image_factory/image_handler.py
@@ -14,8 +14,9 @@ from modules.config.configuration import Configuration
 class ImageHandler(object):
     """
     """
-    def __init__(self, image_file: str, state: Optional[str], config: Configuration):
-        self._image_name = image_file
+    def __init__(self, image_name: str, image_extension: str, state: Optional[str], config: Configuration):
+        self.image_name = image_name
+        self.image_extension = image_extension
         self._state = state
         self._config = config
         self.image_path = self._get_image_path()
@@ -26,13 +27,14 @@ class ImageHandler(object):
         if self._state is None:
             pass
         else:
+            image_file = '.'.join([self.image_name, self.image_extension])
             if self._state == 'raw':
                 image_path = os.path.join(
-                    self._config.raw_images_path, self._image_name
+                    self._config.raw_images_path, image_file
                 )
             else:
                 image_path = os.path.join(
-                    self._config.processed_images_path, self._state, self._image_name
+                    self._config.processed_images_path, self._state, image_file
                 )
         return image_path
 
@@ -40,7 +42,10 @@ class ImageHandler(object):
         self.image = Image.open(self.image_path)
     
     def save(self) -> None:
-        self.image.save(self.image_path, optimize=True)
+        if self.image_extension in ['jpeg', 'JPEG']:
+            self.image.save(self.image_path, 'JPEG', optimize=True)
+        else:
+            self.image.save(self.image_path, optimize=True)
 
     def resize(self, size: Tuple[int, int] = (200, 200)):
         self.image.thumbnail(size, Image.ANTIALIAS)
