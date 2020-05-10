0d3d4b083884ba7c6a3c963502087768a59261cb
Fabien AGNUS <fabien.agnus@sncf.fr>
2020-05-10 12:29:14 +0200

    IMPL: add node color, title and edge title."

--- viz/network_viz/network_vizualizer.py
+++ viz/network_viz/network_vizualizer.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Any, Dict
 
 import pandas as pd
 from pyvis.network import Network
@@ -14,8 +14,10 @@ class NetworkVizualizer(object):
         data: pd.DataFrame,
         source_col: str,
         target_col: str,
+        edge_title_col: str,
         nodes_titles_dict: Dict,
-        edges_titles_dict: Dict,
+        nodes_categories_dict: Dict,
+        categories_color_dict: Dict,
         config: Configuration,
         height: str = '750px',
         width: str = '100%',
@@ -25,8 +27,10 @@ class NetworkVizualizer(object):
         self.data = data
         self.source_col = source_col
         self.target_col = target_col
+        self.edge_title_col = edge_title_col
         self.nodes_titles_dict = nodes_titles_dict
-        self.edges_titles_dict = edges_titles_dict
+        self.nodes_categories_dict = nodes_categories_dict
+        self.categories_color_dict = categories_color_dict
         self._config = config
         self.height = height
         self.width = width
@@ -44,23 +48,47 @@ class NetworkVizualizer(object):
         for _,values in self.data.iterrows():
             source = str(self.nodes_titles_dict.get(values[self.source_col]))
             target = str(self.nodes_titles_dict.get(values[self.target_col]))
+            source_node_color = self._get_node_color(values[self.source_col])
+            target_node_color = self._get_node_color(values[self.target_col])
+            edge_color = get_edge_color(source_node_color, target_node_color)
+            edge_title = values[self.edge_title_col]
             weight = 4
             net_viz.add_node(
                 source,
                 label=source,
-                title=get_node_label_template(source)
+                title=get_node_label_template(source),
+                color=source_node_color
             )
             net_viz.add_node(
                 target,
                 label=target,
-                title=get_node_label_template(target)
+                title=get_node_label_template(target),
+                color=target_node_color
             )
             net_viz.add_edge(
                 source,
                 target,
-                value=5
+                title=edge_title,
+                value=weight,
+                color=edge_color
             )
         self.net_viz = net_viz
     
+    def _get_node_color(self, node_value: Any) -> str:
+        node_color = 'white'
+        node_category = self.nodes_categories_dict.get(node_value)
+        if node_category is not None:
+            color_candidate = self.categories_color_dict.get(node_category)
+            if color_candidate is not None:
+                node_color = color_candidate
+        return node_color
+
     def show(self):
-        self.net_viz.show("essai_perso.html")
+        self.net_viz.show("/home/agnusfabien/Bureau/network.html")
+
+
+def get_edge_color(source_node_color: str, target_node_color: str):
+    edge_color = 'white'
+    if source_node_color == target_node_color:
+        edge_color = source_node_color
+    return edge_color
--- viz/network_viz/popup_templates.py
+++ viz/network_viz/popup_templates.py
@@ -7,7 +7,7 @@ They will be displayed as popups of a network vizualization.
 node_label_template_head = \
   """<head>
 <style>
-artist_name {
+node_label {
 background-color: white;
 font-family: 'proxima_nova_rgbold', Helvetica, Arial, sans-serif;
 font-size: 1.2em;
@@ -17,6 +17,23 @@ font-size: 1.2em;
 
 
 def get_node_label_template(
+    node_label: str,
+    node_label_template_head: str = node_label_template_head
+) -> str:
+    node_label_template = f"""
+<html>
+{node_label_template_head}
+<body>
+
+<node_label>{node_label}</artisnode_labelt_name>
+
+</body>
+</html>
+"""
+    return node_label_template
+
+
+def get_edge_title_template(
     artist_name: str,
     node_label_template_head: str = node_label_template_head
 ) -> str:
